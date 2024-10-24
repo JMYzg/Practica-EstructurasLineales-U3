876b473258a95e98ec695aaa6befef5f0bf607cd
JMYzg <150627399+JMYzg@users.noreply.github.com>
2024-10-24 00:50:01 -0700

    SimplyLinkedList.java

--- SimplyLinkedList.java
+++ SimplyLinkedList.java
@@ -1,157 +1,128 @@
-//Lista de acciones a implementar en la clase:
-// Insertar al final.
-// Insertar en la i-ésima posición.
-// Actualizar el elemento en la i-ésima posición.
-// Consultar el elemento en la i-ésima posición.
-// Eliminar el elemento en la i-ésima posición.
-// Obtener el tamaño de la lista.
-// Consultar si la lista está vacía.
-// Vaciar la lista.
-
-public class SimplyLinkedList<T> extends AbstractList<T> {
+/**
+ * <b>Lista de acciones a implementar en la clase:</b>
+ * <ol>
+ * <li>Insertar al final.
+ * <li>Insertar en la i-ésima posición.
+ * <li>Actualizar el elemento en la i-ésima posición.
+ * <li>Consultar el elemento en la i-ésima posición.
+ * <li>Eliminar el elemento en la i-ésima posición.
+ * <li>Obtener el tamaño de la lista.
+ * <li>Consultar si la lista está vacía.
+ * <li>Vaciar la lista.
+ * </ol>
+ */
+public class SimplyLinkedList<T>
+        extends AbstractList<T> {
     // Atributos
-    int size = 0;
-    Node<T> head = null;
-    Node<T> tail = null;
+    private int size = 0;
+    private Node<T> first = null;
+    private Node<T> last = null;
 
     // Métodos
     @Override
-    public void add(T element) {
+    public void add(T element) { // Insertar al final.
         Node<T> n = new Node<>(element, null);
         if (isEmpty()) {
-            head = n;
+            first = n;
         } else {
-            tail.setNext(n);
+            last.next = n;
         }
-        tail = n;
+        last = n;
         size++;
     }
 
     @Override
-    public void addIn(int index, T element) {
-        if (index < 0 || index > size - 1) {
-            throw new IndexOutOfBoundsException("El índice está fuera de rango: " + index);
-        } else if (index == 0) {
-            Node<T> n = new Node<>(element, null);
-            n.setNext(head);
-            head = n;
-            size++;
+    public void addAt(int index, T element) { // Insertar en la i-ésima posición.
+        isIndexOutOfBound(index);
+        Node<T> n = new Node<>(element, null);
+        if (index == 0) {
+            n.next = first;
+            first = n;
         } else {
-            Node<T> n = new Node<>(element, null);
-            Node<T> aux = head;
-            Node<T> prev = head;
+            Node<T> aux = first;
+            Node<T> prev = aux;
 
             for (int i = 0; i < index; i++) {
                 prev = aux;
-                aux = aux.getNext();
+                aux = aux.next;
             }
-
-            prev.setNext(n);
-            n.setNext(aux);
-            size++;
+            prev.next = n;
+            n.next = aux;
         }
+        size++;
     }
 
     @Override
-    public void update(int index, T element) {
-        if (index < 0 || index > size - 1) {
-            throw new IndexOutOfBoundsException("El índice está fuera de rango: " + index);
-        }
-        else {
-            Node<T> aux = head;
-            Node<T> prev = head;
-            for (int i = 0; i < index; i++) {
-                prev = aux;
-                aux = aux.getNext();
-            }
-            Node<T> n = new Node<>(element, aux.getNext());
-            prev.setNext(n);
-            aux.setNext(null);
-        }
+    public void update(int index, T element) { // Actualizar el elemento en la i-ésima posición.
+        isIndexOutOfBound(index);
+        Node<T> n = first;
+        for (int i = 0; i < index; i++)
+            n = n.next;
+        n.element = element;
     }
 
     @Override
-    public T get(int index) throws IndexOutOfBoundsException {
-        if (index < 0 || index > size - 1) {
-            throw new IndexOutOfBoundsException("El índice está fuera de rango: " + index);
-        }
-        Node<T> n = head; // new Node<>(head.getElement(), head.getNext());
-        for (int i = 0; i < index; i++) {
-            n = n.getNext();
-        }
-        return n.getElement();
+    public T get(int index) throws IndexOutOfBoundsException { // Consultar el elemento en la i-ésima posición.
+        isIndexOutOfBound(index);
+        Node<T> n = first;
+        for (int i = 0; i < index; i++)
+            n = n.next;
+        return n.element;
     }
 
     @Override
-    public void remove(int index) {
-        if (index < 0 || index > size - 1) {
-            throw new IndexOutOfBoundsException("El índice está fuera de rango: " + index);
-        }
-        else if (index == 0) {
-            Node<T> aux = head;
-            head = head.getNext();
-            aux.setNext(null);
-            size--;
+    public void remove(int index) { // Eliminar el elemento en la i-ésima posición.
+        isIndexOutOfBound(index);
+        Node<T> aux = first;
+        if (index == 0) {
+            first = first.next;
         } else {
-            Node<T> aux = head;
-            Node<T> prev = head;
+            Node<T> prev = aux;
             for (int i = 0; i < index; i++) {
                 prev = aux;
-                aux = aux.getNext();
+                aux = aux.next;
             }
-            prev.setNext(aux.getNext());
-            aux.setNext(null);
-            size--;
+            prev.next = aux.next;
         }
+        aux.next = null;
+        size--;
     }
 
     @Override
-    public boolean isEmpty() {
+    public int size() { // Obtener el tamaño de la lista.
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() { // Consultar si la lista está vacía.
         return size <= 0;
     }
 
     @Override
-    public void clear() {
+    public void clear() { // Vaciar la lista.
         if (!isEmpty()) {
             for (int i = 0; i < size - 1; i++) {
-                Node<T> aux = head;
-                head = head.getNext();
-                aux.setNext(null);
+                Node<T> aux = first;
+                first = first.next;
+                aux.next = null;
             }
             size = 0;
         }
     }
 
-//    public T getHead() {
-//        return head.getElement();
-//    }
-//
-//    public T getTail() {
-//        return tail.getElement();
-//    }
-
+    void isIndexOutOfBound(int index) {
+        if (index < 0 || index > size - 1)
+            throw new IndexOutOfBoundsException("El índice está fuera de rango: " + index);
+    }
 
     private static class Node<T> {
         // Atributos
-        private final T element;
-        private Node<T> next;
+        T element;
+        Node<T> next;
 
         // Constructor
         public Node(T element, Node<T> next) {
             this.element = element;
-            this.next = next;
-        }
-
-        //Métodos
-        public T getElement() {
-            return element;
-        }
-
-        public Node<T> getNext() {
-            return next;
-        }
-
-        public void setNext(Node<T> next) {
             this.next = next;
         }
     }
